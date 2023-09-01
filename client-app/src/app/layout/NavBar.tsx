928843d1e08e4eaf412f4fd17181d4d7299ddeb4
Tyson Clark <14clarkt@gmail.com>
2023-08-31 22:16:08 -0400

    refactor(removed Activity access from Navbar)

--- client-app/src/app/layout/NavBar.tsx
+++ client-app/src/app/layout/NavBar.tsx
@@ -12,12 +12,7 @@ export default observer(function NavBar() {
                     <img src="/assets/tcp.png" alt="logo" style={{ marginRight: '10px' }} />
                     Cryobrew
                 </Menu.Item>
-                {/* <Menu.Item as={NavLink} to='/activities' name="Activities" /> */}
                 <Menu.Item as={NavLink} to='/apc' name="APC" />
-                {/* <Menu.Item as={NavLink} to='/errors' name="Errors" /> */}
-                <Menu.Item>
-                    <Button as={NavLink} to='/createActivity' positive content='Create Activity' />
-                </Menu.Item>
                 <Menu.Item position='right'>
                     <Image src={user?.image || '/assets/user.png'} avatar spaced='right' />
                     <Dropdown pointing='top left' text={user?.displayName}>
