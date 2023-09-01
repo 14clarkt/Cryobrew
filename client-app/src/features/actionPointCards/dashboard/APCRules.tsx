23aefd28599a8467c5dca22cb953afb119381aa7
Tyson Clark <14clarkt@gmail.com>
2023-09-01 18:26:25 -0400

    misc(updated APC Rules)

--- client-app/src/features/actionPointCards/dashboard/APCRules.tsx
+++ client-app/src/features/actionPointCards/dashboard/APCRules.tsx
@@ -11,7 +11,7 @@ export default function APCRules() {
                     </Grid.Column>
                     <Grid.Column width={14}>
                         <div>
-                            AP can be used for certain abilities and altering existing abilities and spells. Players use AP Cards to channel their AP to perform specific abilities. Some even provide passive abilities, typically at the cost of reducing your max AP. Coded NPCs also have AP which they use in various ways.
+                            AP can be expended to fuel certain abilities and altering existing abilities and spells. Said abilities and alterations are channeled through AP Cards. Coded NPCs also have AP which they use in various ways.
                         </div>
                     </Grid.Column>
                 </Grid.Row>
@@ -33,7 +33,7 @@ export default function APCRules() {
                     </Grid.Column>
                     <Grid.Column width={14}>
                         <div>
-                            AP Cards are items that only players can interact with. When put into their Aidensphere a member of the alliance can equip the card during a long rest to allow them to use the card's inscribed ability. 
+                            AP Cards are items that only Players can interact with. When put into their Aidensphere any member of the Alliance can equip the card during a Long Rest to allow them to use the card's inscribed abilities.
                         </div>
                     </Grid.Column>
                 </Grid.Row>
@@ -44,7 +44,7 @@ export default function APCRules() {
                     </Grid.Column>
                     <Grid.Column width={14}>
                         <div>
-                            Cards are similar to Crelics, in the sense that they can be upgraded with Code Excess. Some upgrades are strictly better some are just different. When using an APC you can choose which upgrade level of it you wish to use. For Passive APCs you choose which upgrade level you want to be active after a short or long rest.
+                            Cards are similar to Crelics, in the sense that they can be upgraded with Code Excess. Some upgrades are strictly better, some are just different. When using an APC you can choose which upgrade level of it you wish to use. For Passive APCs you choose which upgrade level you want to be active after a short or long rest.
                         </div>
                     </Grid.Column>
                 </Grid.Row>
@@ -55,7 +55,7 @@ export default function APCRules() {
                     </Grid.Column>
                     <Grid.Column width={14}>
                         <div>
-                            Cards can be found typically from defeating enemies and using your Aidensphere on them, looting Player Chests, 
+                            Cards can be found typically from defeating enemies and using your Aidensphere on them, looting Player Chests, etc.
                         </div>
                     </Grid.Column>
                 </Grid.Row>
@@ -66,7 +66,7 @@ export default function APCRules() {
                     </Grid.Column>
                     <Grid.Column width={14}>
                         <div>
-                            Every Player starts with the Self Heal APC. If you take a level in the Martial class you get the Last Stand APC as well. If you take a level granting you the "Spellcasting" class feature, you get the Pool of Power class feature. (Perhaps martial also get an APC to use proficiency dice instead of bonus for non spell attack rolls)
+                            Every Player starts with the Self Heal APC. Players may choose to also start with also the Heroic Hitter, Last Stand, or Pool of Power APCs.
                         </div>
                     </Grid.Column>
                 </Grid.Row>
@@ -77,7 +77,7 @@ export default function APCRules() {
                     </Grid.Column>
                     <Grid.Column width={14}>
                         <div>
-                            You can equip an APC for every APC Slot you have. Every Character has a number of APC Slot equal to their proficiency bonus minus 1. Additionally, characters gain an additional slot for every 5 levels in martial classes.
+                            You can equip an APC for every APC Slot you have. Every Player has a number of APC Slot equal to their Proficiency Bonus. Additionally, Players gain an additional slot for every 5 levels in Martial Classes.
                         </div>
                     </Grid.Column>
                 </Grid.Row>
@@ -88,7 +88,7 @@ export default function APCRules() {
                     </Grid.Column>
                     <Grid.Column width={14}>
                         <div>
-                            You regain all AP after a long rest. You regain 1 AP per martial class level after a short rest.
+                            You regain all AP after a long rest. You regain 1 AP per Martial Class level after a Short Rest.
                         </div>
                     </Grid.Column>
                 </Grid.Row>
@@ -110,7 +110,7 @@ export default function APCRules() {
                     </Grid.Column>
                     <Grid.Column width={14}>
                         <div>
-                            If a level of an APC has a limit on uses or choices, any use of a level of that APC counts as a use of that limitation for all levels. For example: if you use a level that says "Once per round" and try to use another that says "Twice per round", the once per round limitation supersedes any other use of this ability, and it can't be used.*
+                            If a level of an APC has a limit on uses or choices, any use of a level of that APC counts as a use of that limitation for all levels. For example: if you use a level that says "Once per round" and try to use another that says "Twice per round", the once per round limitation supersedes any other use of this ability, and it can't be used.
                         </div>
                     </Grid.Column>
                 </Grid.Row>
