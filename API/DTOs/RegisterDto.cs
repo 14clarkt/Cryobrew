ddd7bb2db6fc36ad751b3fa80a4ab9a44b232867
14clarkt <14clarkt@gmail.com>
2023-09-05 23:57:55 -0400

    Fix(Passwords could only be between length 4-8, and gave a poor error message)

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -10,7 +10,7 @@ namespace API.DTOs
         [EmailAddress]
         public string Email { get; set; }
         [Required]
-        [RegularExpression("(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{4,8}$", ErrorMessage = "Password must be complex")]
+        [RegularExpression("(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{8,32}$", ErrorMessage = "Password must have length between 8-32, contain at least 1 symbol. and contain at least 1 uppercase letter.")]
         public string Password { get; set; }
         [Required]
         public string Username { get; set; }
