217d1b3644728d2e1330d61209ed37777bdd4b20
14clarkt <14clarkt@gmail.com>
2023-11-08 20:10:25 -0500

    Style(Sidebar Toggle Button style)

--- client-app/src/app/layout/NavBar.tsx
+++ client-app/src/app/layout/NavBar.tsx
@@ -4,16 +4,17 @@ import { observer } from 'mobx-react-lite';
 import { useStore } from '../stores/store';
 import AccountOptions from '../common/options/AccountOptions';
 export default observer(function NavBar() {
-    const { commonStore: { toggleSidebar } } = useStore();
+    const { commonStore: { toggleSidebar, sidebarVisable } } = useStore();
     return (
         <Menu inverted fixed='top'>
-            <Container>
+            <Container fluid>
+                <Menu.Item as={Button} name="Sidebar" onClick={toggleSidebar} active={sidebarVisable}/>
+                
+                <Menu.Item style={{ paddingLeft:"250px" }}/>
+                
                 <Menu.Item as={NavLink} to='/' header>
                     <img src="/assets/tcp.png" alt="logo" style={{ marginRight: '10px' }} />
                     Cryobrew
-                </Menu.Item>
-                <Menu.Item>
-                    <Button fluid inverted color='teal' content="Sidebar" onClick={toggleSidebar} />
                 </Menu.Item>
                 <Menu.Item as={NavLink} to='/apc' name="APC" />
                 <Menu.Item as={NavLink} to='/alchemy' name="Alchemy" />
