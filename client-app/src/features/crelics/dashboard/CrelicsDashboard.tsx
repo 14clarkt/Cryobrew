624cc64bff281381c14455f5a0d6ebb16836c1c9
14clarkt <14clarkt@gmail.com>
2023-11-09 23:13:32 -0500

    Feature(Added tabs to future features to implement)

---
+++ client-app/src/features/crelics/dashboard/CrelicsDashboard.tsx
@@ -0,0 +1,7 @@
+import { observer } from "mobx-react-lite"
+
+export default observer(function CrelicsDashboard() {
+    return (
+        <div style={{color:"white"}}>Crelics details and upgrades. To be implemented</div>
+    )
+})
---
+++ client-app/src/features/crystals/dashboard/CrystalsDashboard.tsx
@@ -0,0 +1,7 @@
+import { observer } from "mobx-react-lite"
+
+export default observer(function CrystalsDashboard() {
+    return (
+        <div style={{color:"white"}}>Crystal item details and upgrades. To be implemented</div>
+    )
+})
