1af04af4e74818343bcecf11986c3bdc037d2b89
14clarkt <14clarkt@gmail.com>
2023-11-10 23:14:40 -0500

    Style(DiffSpan now has dynamic colors)

--- client-app/src/app/common/display/DiffSpan.tsx
+++ client-app/src/app/common/display/DiffSpan.tsx
@@ -1,16 +1,49 @@
+import { generateKey } from "../functions/CommonFunctions";
+
 interface Props {
     content: string;
 }
 
 export default function DiffSpan(props: Props) {
-    const contentArray = props.content.split("\\*");
+    const colorMap = new Map<string, string>([
+        ["yellow", "\\y"],
+        ["red", "\\r"],
+        ["lightGreen", "\\g"],
+        ["cyan", "\\c"],
+        ["orange", "\\o"],
+    ])
+
+    const colorizeSpan = (text: string): JSX.Element[] => {
+        let earliestSet: [string, number] = ["none", -1]
+
+        colorMap.forEach((value, key) => {
+            let colorIndex = text.indexOf(value)
+            if (colorIndex !== -1 && (earliestSet[1] === -1 || earliestSet[1] > colorIndex)) {
+                earliestSet = [key, colorIndex]
+            }
+        })
+
+        if (earliestSet[1] === -1)
+            return [<span key={text}>{text}</span>]
+
+        const splitSymbol = colorMap.get(earliestSet[0])!
+        const textArray = text.split(splitSymbol);
+        
+        if (textArray.length > 2) {
+            //Split at the second occurance of the splitsymbol and run this again on that split.
+            let colorIndex = text.indexOf(splitSymbol)
+            let recurssionText = text.slice(colorIndex + splitSymbol.length)
+            colorIndex = recurssionText.indexOf(splitSymbol)
+            recurssionText = recurssionText.slice(colorIndex + splitSymbol.length)
+            return [<span key={generateKey('ds1')}>{textArray[0]}</span>, <span key={generateKey('ds2')} style={{ color: earliestSet[0] }}>{textArray[1]}</span>, ...colorizeSpan(recurssionText)]
+        } else {
+            return [<span key={generateKey('ds3')}>{textArray[0]}</span>, <span key={generateKey('ds4')} style={{ color: earliestSet[0] }}>{textArray[1]}</span>]
+        }
+    }
 
     return (
-        <span>
-            {contentArray.map((val, ind) => 
-                (ind % 2 === 0) ? <span key={ind}>{val}</span>
-                : <span style={{color: "yellow"}}key={ind}>{val}</span>
-            )}
+        <span key={generateKey('ds')}>
+            {colorizeSpan(props.content)}
         </span>
     )
 }
