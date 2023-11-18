6ebe9c2a30870d57ab1fc43841f7a5973d4bc81d
14clarkt <14clarkt@gmail.com>
2023-11-18 18:45:42 -0500

    Style(Dice Roller Input fields only allow numbers to be typed in)

--- client-app/src/features/misc/diceRoller/DiceRoller.tsx
+++ client-app/src/features/misc/diceRoller/DiceRoller.tsx
@@ -5,7 +5,7 @@ import { Button, Grid, Input } from "semantic-ui-react"
 export default observer(function DiceRoller() {
     const [min, setMin] = useState<string>("")
     const [max, setMax] = useState<string>("")
-    const [result, setResult] = useState<string>("")
+    const [result, setResult] = useState<string>("0")
 
     const updateMin = (data: string) => {
         setMin(data)
@@ -29,27 +29,43 @@ export default observer(function DiceRoller() {
             !isNaN(Number(value.toString())));
     }
 
-    return (<>
-        <Grid>
+    return (
+        <Grid inverted divided="vertically">
             <Grid.Row>
-                <Grid.Column width={5}>
-                    <Input fluid placeholder="min" style={{ backgroundColor: "#222222", color: "white" }} onChange={(_e, data) => updateMin(data.value)} />
+                <Grid.Column width={8}>
+                    <Input onKeyPress={(event: { key: string; preventDefault: () => void }) => {
+                        if (!/[0-9]/.test(event.key)) {
+                            event.preventDefault();
+                        }
+                    }} fluid placeholder="min" style={{ backgroundColor: "#222222", color: "white" }} onChange={(_e, data) => updateMin(data.value)} />
                 </Grid.Column>
-                <Grid.Column width={5}>
-                    <Input fluid placeholder="max" style={{ backgroundColor: "#222222", color: "white" }} onChange={(_e, data) => updateMax(data.value)} />
-                </Grid.Column>
-                <Grid.Column width={6}>
-                    <h1 style={{ color: "white" }}>res: {result}</h1>
+                <Grid.Column width={8}>
+                    <Input onKeyPress={(event: { key: string; preventDefault: () => void }) => {
+                        if (!/[0-9]/.test(event.key)) {
+                            event.preventDefault();
+                        }
+                    }} fluid placeholder="max" style={{ backgroundColor: "#222222", color: "white" }} onChange={(_e, data) => updateMax(data.value)} />
                 </Grid.Column>
             </Grid.Row>
             <Grid.Row>
-                <Button
-                    content="Roll"
-                    color="green"
-                    fluid inverted
-                    onClick={roll}
-                />
+                <Grid.Column width={4} />
+                <Grid.Column width={8}>
+                    <h1 style={{ textAlign: "center", color: "white" }}>{result}</h1>
+                </Grid.Column>
+                <Grid.Column width={4} />
             </Grid.Row>
-        </Grid>
-    </>)
+            <Grid.Row>
+                <Grid.Column width={4} />
+                <Grid.Column width={8} >
+                    <Button
+                        content="Roll"
+                        color="green"
+                        fluid inverted
+                        onClick={roll}
+                    />
+                </Grid.Column>
+                <Grid.Column width={4} />
+            </Grid.Row>
+        </Grid >
+    )
 })
