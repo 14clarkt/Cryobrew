4bcfd7f4f68c41fb70d98685d26e07095c97fab0
14clarkt <14clarkt@gmail.com>
2024-02-15 10:04:36 -0500

    Style (Enchanting Search Bar on right side)

--- client-app/src/features/enchanting/dashboard/EnchantingHeader.tsx
+++ client-app/src/features/enchanting/dashboard/EnchantingHeader.tsx
@@ -1,27 +1,16 @@
 import { observer } from 'mobx-react-lite';
-import { Grid, Search, Segment } from 'semantic-ui-react';
-import { useStore } from '../../../app/stores/store';
+import { Grid } from 'semantic-ui-react';
 
 export default observer(function EnchantingHeader() {
-    const { enchantingStore } = useStore()
-    const { setEnchFilter } = enchantingStore
     return (
-        <Segment style={{ backgroundColor: "black" }}>
-            <Grid>
-                <Grid.Row>
-                    <Grid.Column width='4' />
-                    <Grid.Column width='8'>
-                        <h1 style={{ color: 'cyan', textAlign: 'center' }}>Enchantments</h1>
-                    </Grid.Column>
-                    <Grid.Column width='4' >
-                        <Search
-                            onSearchChange={(_e, data) => { data.value ? setEnchFilter(data.value) : setEnchFilter("") }}
-                            open={false}
-                            placeholder='Search'
-                        />
-                    </Grid.Column>
-                </Grid.Row>
-            </Grid>
-        </Segment>
+        <Grid>
+            <Grid.Row>
+                <Grid.Column width='4' />
+                <Grid.Column width='8'>
+                    <h1 style={{ color: 'cyan', textAlign: 'center' }}>Enchantments</h1>
+                </Grid.Column>
+                <Grid.Column width='4' />
+            </Grid.Row>
+        </Grid>
     )
 })
