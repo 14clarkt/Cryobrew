67fdd45791d637f429aa08a8c13488764182fb46
14clarkt <14clarkt@gmail.com>
2024-04-03 16:56:14 -0400

    Feature (Add Cost to CrelicSubAbilityLevel)

--- Domain/crelics/CrelicSubAbilityLevel.cs
+++ Domain/crelics/CrelicSubAbilityLevel.cs
@@ -5,5 +5,6 @@ namespace Domain
         public Guid Id { get; set; }
         public string Description { get; set; }
         public int Level { get; set; }
+        public int Cost { get; set; }
     }
 }
