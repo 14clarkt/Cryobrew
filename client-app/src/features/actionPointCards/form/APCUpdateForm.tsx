fb08839cc075d4f9b1327f0eddb2bd98a56ceff8
14clarkt <14clarkt@gmail.com>
2024-10-11 11:17:59 -0400

    Feature (Force Unequip APCs)

--- client-app/src/features/actionPointCards/form/APCUpdateForm.tsx
+++ client-app/src/features/actionPointCards/form/APCUpdateForm.tsx
@@ -1,11 +1,12 @@
 import { ErrorMessage, Form, Formik } from "formik";
-import { Button } from "semantic-ui-react";
+import { Button, Grid } from "semantic-ui-react";
 import { observer } from "mobx-react-lite";
 import { useStore } from "../../../app/stores/store";
 import MyTextInput from "../../../app/common/form/MyTextInput";
 import { ActionPointCard } from "../../../app/models/actionPointCard";
 import * as Yup from 'yup';
 import ValidationErrors from "../../errors/ValidationErrors";
+import { useState } from "react";
 
 interface Props {
     apc: ActionPointCard
@@ -14,12 +15,13 @@ interface Props {
 export default observer(function APCUpdateForm(props: Props) {
     const { apcStore } = useStore()
     const { apc: oldApc } = props
+    const [unequipped, setUnequipped] = useState(false)
 
     return (
         <Formik
             initialValues={{ name: oldApc.name, error: null }}
             onSubmit={(values, { setErrors }) => {
-                let newApc = {...oldApc, name: values.name}
+                let newApc = { ...oldApc, name: values.name }
                 apcStore.updateApc(newApc).catch(error =>
                     setErrors({ error }))
             }}
@@ -34,14 +36,28 @@ export default observer(function APCUpdateForm(props: Props) {
                         name='error' render={() =>
                             <ValidationErrors errors={errors.error} />}
                     />
-                    <Button
-                        disabled={!isValid || !dirty || isSubmitting}
-                        content="Rename"
-                        type="submit"
-                        color = "green"
-                        fluid inverted
-                        loading={apcStore.loading}
-                    />
+                    <Grid>
+                        <Grid.Column width={8}>
+                            <Button
+                                disabled={!isValid || !dirty || isSubmitting}
+                                content="Rename"
+                                type="submit"
+                                color="green"
+                                fluid inverted
+                                loading={apcStore.loading}
+                            />
+                        </Grid.Column>
+                        <Grid.Column width={8}>
+                            <Button
+                                disabled={!oldApc.equippedBy || unequipped}
+                                content="Unequip"
+                                type="button"
+                                color="yellow"
+                                fluid inverted loading={apcStore.loading}
+                                onClick={async () => {await apcStore.equipApc(oldApc, oldApc.equippedBy!); setUnequipped(true)}}
+                            />
+                        </Grid.Column>
+                    </Grid>
                 </Form>
             )}
         </Formik>
